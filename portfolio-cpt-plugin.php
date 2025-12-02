e7c985ebfc08535754ca98edb60b74d4285583b5
Mella <melisa.debartolo@gmail.com>
2025-12-02 11:31:11 +0100

    gitignore and main php file

---
+++ portfolio-cpt-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Portfolio CPT Plugin
+ * Description: Custom post types for my portfolio
+ * Version: 1.0.0
+ * Author: Mella
+ * Text Domain: portfolio-cpt
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
