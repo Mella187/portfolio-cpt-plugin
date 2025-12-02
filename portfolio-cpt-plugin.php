66ba8f0cf658d54a946105f522e092a2a0bf2ecf
Mella <melisa.debartolo@gmail.com>
2025-12-02 12:03:57 +0100

    Register CPT and Taxonomies

--- portfolio-cpt-plugin.php
+++ portfolio-cpt-plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Portfolio CPT Plugin
  * Description: Custom post types for my portfolio
@@ -10,3 +11,52 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
+
+/** Register CPT */
+
+function portfolio_register_post_types()
+{
+    register_post_type('project', array(
+        'labels' => array(
+            'name' => 'Projects',
+            'singular_name' => 'Project',
+            'add_new' => 'Add New Project',
+            'edit_item' => 'Edit Project',
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'menu_icon' => 'dashicons-portfolio',
+        'supports' => array('title', 'editor', 'thumbnail', 'excerpt'),
+    ));
+}
+
+add_action('init', 'portfolio_register_post_types');
+
+
+/** Register Custom Taxonomies */
+
+function portfolio_register_taxonomies()
+{
+    register_taxonomy('project_type', 'project', array(
+        'labels' => array(
+            'name' => 'Project Types',
+            'singular_name' => 'Project Type',
+        ),
+        'public' => true,
+        'hierarchical' => true,
+        'show_in_rest' => true,
+    ));
+
+    register_taxonomy('project_tag', 'project', array(
+        'labels' => array(
+            'name' => 'Project Tags',
+            'singular_name' => 'Project Tag',
+        ),
+        'public' => true,
+        'hierarchical' => false,
+        'show_in_rest' => true,
+    ));
+}
+add_action('init', 'portfolio_register_taxonomies');
